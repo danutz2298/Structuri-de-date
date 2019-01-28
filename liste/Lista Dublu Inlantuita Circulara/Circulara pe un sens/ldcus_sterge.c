c3aeacdc50921c49cbe7d867d4df53db6f12d121
Gabi Dinu <gabi@Gabis-MacBook-Pro.local>
2019-01-28 11:25:47 +0200

    update

---
+++ liste/Lista Dublu Inlantuita Circulara/Circulara pe un sens/ldcus_sterge.c
@@ -0,0 +1,127 @@
+// Program ce creeaza o lista dublu inlantuita circulara pe un sens si sterge elemente din ea
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structura lista dublu inlantuita
+struct node{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+// Functie ce adauga in lista un element nou
+void push_ldcus(struct node **head,int data){
+    struct node *new_node, *ptr,*frst;
+    new_node = (struct node*)malloc(sizeof(struct node));
+
+    if(*head == NULL){
+        new_node->prev = NULL;
+        new_node->data = data;
+        new_node->next = new_node;
+        *head = new_node;
+    }else{
+        ptr = *head;
+        frst = *head;
+        while(ptr->next != *head){
+            ptr = ptr->next;
+        }
+        new_node->data = data;
+
+        new_node->prev = NULL;
+        new_node->next = frst;
+        frst->prev = new_node;
+        ptr->next = new_node;
+        *head = new_node;
+    }
+}
+
+void afiseaza_ldcus(struct node *head){
+    if(head == NULL){
+        printf("Lista este goala!\n");
+        exit(0);
+    }
+
+    struct node *ptr;
+    ptr = head;
+
+    do{
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }while(ptr->next != head);
+    if(ptr->prev != NULL && ptr->next == head){
+       printf("%d ", ptr->data);
+    }
+}
+
+// Functie ce sterge primul nod din lista din lista
+void remove_front_ldCus(struct node **head){
+    struct node *first,*ptr,*ptr2;
+    ptr = *head;
+    first = *head;
+    ptr2 = ptr->next;
+
+    // Daca lista este goala
+    if(ptr->prev == NULL && ptr->next == NULL){
+            *head = NULL;
+            return;
+    }
+
+    if(first->prev == NULL){
+        while(ptr->next != *head){
+            ptr = ptr->next;
+        }
+        ptr->next = first->next;
+        ptr2->prev = NULL;
+        *head = first->next;
+        free(first);
+    }
+}
+
+// Functie ce sterge un nod din lista
+void remove_node_ldcus(struct node **head, int value){
+    struct node *ptr,*ptr2,*ptr3;
+    ptr = *head;
+    ptr2 = ptr;
+    ptr3 = ptr2;
+
+    if(ptr->data == value){
+        if(ptr->prev == NULL && ptr->next == ptr){
+            free(ptr);
+            *head = NULL;
+            return;
+        }
+    }
+
+    while(ptr->data != value && ptr->next != *head){
+            ptr2 = ptr;
+            ptr = ptr->next;
+            ptr3 = ptr->next;;
+    }
+
+    if(ptr->data == value){
+        if(ptr->prev == NULL){
+            remove_front_ldCus(head);
+        }else if(ptr->next == ptr){
+
+        }else{
+            ptr2->next = ptr3;
+            ptr3->prev = ptr2;
+            free(ptr);
+        }
+    }
+
+}
+
+int main(){
+    struct node *head = NULL; // pointer catre primul nod din lista
+
+    // Adaugam elemente in lista
+    push_ldcus(&head,3);
+    push_ldcus(&head,4);
+    push_ldcus(&head,45);
+
+    printf("elementele listei dupa stergere sunt:");
+    remove_node_ldcus(&head,3);
+    afiseaza_ldcus(head);
+    printf("\n");
+}
